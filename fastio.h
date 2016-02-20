1df8b936c0aaf8b4c2dcdbc83a15cd3ab57aca64
Matteo Geromin <geromin.matteo@gmail.com>
2016-02-20 16:04:07 +0100

    Update GPIO fastio macros

--- fastio.h
+++ fastio.h
@@ -6,16 +6,7 @@
 #ifndef	_FASTIO_ARDUINO_H
 #define	_FASTIO_ARDUINO_H
 
-//#include <avr/io.h>
-
-/*
-  utility functions
-*/
-
-#ifndef MASK
-/// MASKING- returns \f$2^PIN\f$
-#define MASK(PIN)  (1 << PIN)
-#endif
+#include "gpio_lib.h"
 
 /*
   magic I/O routines
@@ -23,37 +14,20 @@
 */
 
 /// Read a pin
-#define _READ(IO) 0//((bool)(DIO ## IO ## _RPORT & MASK(DIO ## IO ## _PIN)))
+#define _READ(IO) get_gpio_value(IO)
 /// write to a pin
-// On some boards pins > 0x100 are used. These are not converted to atomic actions. An critical section is needed.
-
-#define _WRITE_NC(IO, v)  do{}while(0)//do { if (v) {DIO ##  IO ## _WPORT |= MASK(DIO ## IO ## _PIN); } else {DIO ##  IO ## _WPORT &= ~MASK(DIO ## IO ## _PIN); }; } while (0)
-
-#define _WRITE_C(IO, v)   do{}while(0)//do { if (v) { \
-                                         CRITICAL_SECTION_START; \
-                                         {DIO ##  IO ## _WPORT |= MASK(DIO ## IO ## _PIN); }\
-                                         CRITICAL_SECTION_END; \
-                                       }\
-                                       else {\
-                                         CRITICAL_SECTION_START; \
-                                         {DIO ##  IO ## _WPORT &= ~MASK(DIO ## IO ## _PIN); }\
-                                         CRITICAL_SECTION_END; \
-                                       }\
-                                     }\
-                                     while (0)
-
-#define _WRITE(IO, v)  do{}while(0)//do {  if (&(DIO ##  IO ## _RPORT) >= (uint8_t *)0x100) {_WRITE_C(IO, v); } else {_WRITE_NC(IO, v); }; } while (0)
+#define _WRITE(IO, v) set_gpio_value(IO, (PIN_VALUE)v)
 
 /// toggle a pin
-#define _TOGGLE(IO)  do{}while(0)//do {DIO ##  IO ## _RPORT = MASK(DIO ## IO ## _PIN); } while (0)
+#define _TOGGLE(IO) set_gpio_value(IO, (PIN_VALUE)(get_gpio_value(IO)? 0:1))
 
 /// set pin as input
-#define	_SET_INPUT(IO) do{}while(0)//do {DIO ##  IO ## _DDR &= ~MASK(DIO ## IO ## _PIN); } while (0)
+#define	_SET_INPUT(IO) set_gpio_input(IO)
 /// set pin as output
-#define	_SET_OUTPUT(IO) do{}while(0)//do {DIO ##  IO ## _DDR |=  MASK(DIO ## IO ## _PIN); } while (0)
+#define	_SET_OUTPUT(IO) set_gpio_output(IO, (PIN_VALUE)get_gpio_value(IO))
 
 /// check if pin is an input
-#define	_GET_INPUT(IO)  0//((DIO ## IO ## _DDR & MASK(DIO ## IO ## _PIN)) == 0)
+#define	_GET_INPUT(IO) 0//((DIO ## IO ## _DDR & MASK(DIO ## IO ## _PIN)) == 0)
 /// check if pin is an output
 #define	_GET_OUTPUT(IO)  0//((DIO ## IO ## _DDR & MASK(DIO ## IO ## _PIN)) != 0)
 
@@ -65,7 +39,7 @@
 /// Read a pin wrapper
 #define READ(IO)  _READ(IO)
 /// Write to a pin wrapper
-#define WRITE(IO, v)  _WRITE(IO, v)
+#define WRITE(IO, v)  _WRITE(IO, (PIN_VALUE)v)
 
 /// toggle a pin wrapper
 #define TOGGLE(IO)  _TOGGLE(IO)
